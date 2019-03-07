4caa03bf068b9724c8bbe777295d441114523fa9
Michael Wnek <michaelwnek1@gmail.com>
2019-03-07 21:42:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,7 +6,11 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
 end
 
 def apply_clearance(cart)
