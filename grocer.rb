1a2347a454c76e1714171cc4dd0ce7d406a8bf6f
Michael Wnek <michaelwnek1@gmail.com>
2019-03-07 21:50:01 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,34 @@
 def consolidate_cart(cart)
-  # code here
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
 end
 
 def apply_clearance(cart)
-  # code here
+  {
+  "PEANUTBUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
+}
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  total = 0
+  final_cart.each do |name, properties|
+    total += properties[:price] * properties[:count]
+  end
+  total = total * 0.9 if total > 100
+  total
 end
